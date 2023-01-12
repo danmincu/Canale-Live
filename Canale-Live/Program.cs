433763b38952ac5110b2b4a8c20194d9ea9382dd
Dan Mincu <danmincu@gmail.com>
2023-01-12 03:30:24 -0500

    fix

--- Canale-Live/Program.cs
+++ Canale-Live/Program.cs
@@ -1,19 +1,29 @@
 using Canale_Live.Controllers;
 using Canale_Live.Controllers.Getters;
+using Microsoft.Extensions.Configuration.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configPath = Environment.GetEnvironmentVariable("SETTINGS_CONFIG_PATH");
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IProxyGetter, ProxyGetter>();
 builder.Services.AddSingleton<IRedirectCollection, Redirects>();
-builder.Configuration.AddJsonFile("appsettings.json");
+
+
+var fileName = !string.IsNullOrEmpty(configPath) ? Path.Combine(configPath, "appsettings.json") : "appsettings.json";
+Console.WriteLine(fileName);
+var tbr = builder.Configuration.Sources.OfType<JsonConfigurationSource>().Where(p => p.Path == "appsettings.json").FirstOrDefault();
+if (tbr != null) builder.Configuration.Sources.Remove(tbr!);
+builder.Configuration.AddJsonFile(fileName, optional: false, reloadOnChange: true);
+
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 builder.Logging.AddAzureWebAppDiagnostics();
 
 var app = builder.Build();
+
 
 System.Net.ServicePointManager.DefaultConnectionLimit = 30;
 
